8bb00f155a7e7d41b4014a105f811a579064875c
brian054 <brian.rowell54@gmail.com>
2024-01-30 21:52:51 -0500

    First

---
+++ constants.js
@@ -0,0 +1,9 @@
+import * as ex from "excalibur";
+
+export const VIEWPORT_WIDTH = 160 + 48;
+export const VIEWPORT_HEIGHT = 144 + 48;
+
+export const SCALE = 2;
+export const SCALE_2x = new ex.Vector(2, 2);
+
+// add more constants
